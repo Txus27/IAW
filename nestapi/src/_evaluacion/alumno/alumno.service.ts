47273f9dd03d45af85337bc1189a83e1f1df0483
TxusGamer027 <juanlusanchez27@gmail.com>
2025-02-23 15:30:24 +0100

    Fin tarea

--- nestapi/src/_evaluacion/alumno/alumno.service.ts
+++ nestapi/src/_evaluacion/alumno/alumno.service.ts
@@ -33,39 +33,3 @@ export class AlumnoService {
     return `Alunmo ${id} no encontrado`;
   }
 }
-
-
-
-
-
-
-
-
-
-
-// import { Injectable } from '@nestjs/common';
-// import { CreateAlumnoDto } from './dto/create-alumno.dto';
-// import { UpdateAlumnoDto } from './dto/update-alumno.dto';
-
-// @Injectable()
-// export class AlumnoService {
-//   create(createAlumnoDto: CreateAlumnoDto) {
-//     return 'This action adds a new alumno';
-//   }
-
-//   findAll() {
-//     return `This action returns all alumno`;
-//   }
-
-//   findOne(id: number) {
-//     return `This action returns a #${id} alumno`;
-//   }
-
-//   update(id: number, updateAlumnoDto: UpdateAlumnoDto) {
-//     return `This action updates a #${id} alumno`;
-//   }
-
-//   remove(id: number) {
-//     return `This action removes a #${id} alumno`;
-//   }
-// }
